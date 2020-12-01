faa27a72d1a8a634dfd037250f7b1bc0d3bd2bfa
Unero <ramadhanbhg@gmail.com>
2020-12-01 10:43:04 +0700

    Initial commit

---
+++ app/src/main/java/com/unero/roomexample/recycler/WordListAdapter.kt
@@ -0,0 +1,51 @@
+package com.unero.roomexample.recycler
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.unero.roomexample.R
+import com.unero.roomexample.repository.Word
+
+class WordListAdapter : ListAdapter<Word, WordViewHolder>(WORDS_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.word)
+    }
+
+    class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val wordItemView: TextView = itemView.findViewById(R.id.textView)
+
+        fun bind(text: String?) {
+            wordItemView.text = text
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): WordViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recyclerview_item, parent, false)
+                return WordViewHolder(view)
+            }
+        }
+    }
+
+    companion object {
+        private val WORDS_COMPARATOR = object : DiffUtil.ItemCallback<Word>() {
+            override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
+                return oldItem.word == newItem.word
+            }
+        }
+    }
+}
